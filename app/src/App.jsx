2c38266dab51dbbd98b35e5ec8fada974664b27d
Paul Thomas <paulthomas2107@gmail.com>
2023-01-29 11:39:50 +0000

    More

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,35 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import reactLogo from './assets/react.svg';
 import Editor from '@monaco-editor/react';
+import * as Y from 'yjs';
+import { WebrtcProvider } from 'y-webrtc';
+import { MonacoBinding } from 'y-monaco';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const editorRef = useRef(null);
 
-  return <Editor height="100vh" width="100vw" theme="vs-dark" />;
+  function handleEditorDidMount(editor, monaco) {
+    editorRef.current = editor;
+    const doc = new Y.Doc();
+    const provider = new WebrtcProvider('test-room', doc);
+    const type = doc.getText('monaco');
+    const binding = new MonacoBinding(
+      type,
+      editorRef.current.getModel(),
+      new Set([editorRef.current]),
+      provider.awareness
+    );
+    console.log(provider.awareness);
+  }
+
+  return (
+    <Editor
+      height="100vh"
+      width="100vw"
+      theme="vs-dark"
+      onMount={handleEditorDidMount}
+    />
+  );
 }
 
 export default App;
